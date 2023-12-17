d1f1597bb9acf6c71988dad331aa1cddabe9065f
Sufiyan Ansar Shaikh <Sufiyan.Shaikh@edynamic.net>
2023-12-17 23:03:42 +0530

    practice

---
+++ practice/index.js
@@ -0,0 +1,195 @@
+const userData = {
+  headline: "Designed for You",
+  "secondary-tabs": [
+    {
+      tab: "Open Living",
+      image: {
+        src: "http://res.cloudinary.com/hugeinc/image/upload/model-education_x0wqje",
+        alt: "Open Living",
+        caption: "Open Living",
+      },
+      headline: "Open Living",
+      description:
+        "At Pulte, we build our homes with you in mind. Every inch was thoughtfully designed to best meet your family’s needs making your life better, happier and easier. Simply put, you can do more in a Pulte home, because we’ve built in all the ways to get more out of life.",
+    },
+    {
+      tab: "Everyday Entry Living",
+      image: {
+        src: "http://res.cloudinary.com/hugeinc/image/upload/model-education_x0wqje",
+        alt: "Everyday Entry Living",
+        caption: "Everyday Entry Living",
+      },
+      headline: "Everyday Entry Living",
+      description:
+        "At Pulte, we build our homes with you in mind. Every inch was thoughtfully designed to best meet your family’s needs making your life better, happier and easier. Simply put, you can do more in a Pulte home, because we’ve built in all the ways to get more out of life.",
+    },
+    {
+      tab: "Family Bathroom",
+      image: {
+        src: "http://res.cloudinary.com/hugeinc/image/upload/model-education_x0wqje",
+        alt: "Family Bathroom",
+        caption: "Family Bathroom",
+      },
+      headline: "Family Bathroom",
+      description:
+        "At Pulte, we build our homes with you in mind. Every inch was thoughtfully designed to best meet your family’s needs making your life better, happier and easier. Simply put, you can do more in a Pulte home, because we’ve built in all the ways to get more out of life.",
+    },
+    {
+      tab: "HomeBase",
+      image: {
+        src: "http://res.cloudinary.com/hugeinc/image/upload/model-education_x0wqje",
+        alt: "HomeBase",
+        caption: "HomeBase",
+      },
+      headline: "HomeBase",
+      description:
+        "At Pulte, we build our homes with you in mind. Every inch was thoughtfully designed to best meet your family’s needs making your life better, happier and easier. Simply put, you can do more in a Pulte home, because we’ve built in all the ways to get more out of life.",
+    },
+    {
+      tab: "LifeTested Living Spaces",
+      image: {
+        src: "http://res.cloudinary.com/hugeinc/image/upload/model-education_x0wqje",
+        alt: "LifeTested Living Spaces",
+        caption: "LifeTested Living Spaces",
+      },
+      headline: "LifeTested Living Spaces",
+      description:
+        "At Pulte, we build our homes with you in mind. Every inch was thoughtfully designed to best meet your family’s needs making your life better, happier and easier. Simply put, you can do more in a Pulte home, because we’ve built in all the ways to get more out of life.",
+    },
+    {
+      tab: "Oversized Pantry",
+      image: {
+        src: "http://res.cloudinary.com/hugeinc/image/upload/model-education_x0wqje",
+        alt: "Oversized Pantry",
+        caption: "Oversized Pantry",
+      },
+      headline: "Oversized Pantry",
+      description:
+        "At Pulte, we build our homes with you in mind. Every inch was thoughtfully designed to best meet your family’s needs making your life better, happier and easier. Simply put, you can do more in a Pulte home, because we’ve built in all the ways to get more out of life.",
+    },
+    {
+      tab: "Owner’s Retreat",
+      image: {
+        src: "http://res.cloudinary.com/hugeinc/image/upload/model-education_x0wqje",
+        alt: "Owner’s Retreat",
+        caption: "Owner’s Retreat",
+      },
+      headline: "Owner’s Retreat",
+      description:
+        "At Pulte, we build our homes with you in mind. Every inch was thoughtfully designed to best meet your family’s needs making your life better, happier and easier. Simply put, you can do more in a Pulte home, because we’ve built in all the ways to get more out of life.",
+    },
+    {
+      tab: "Pulte Planning Center",
+      image: {
+        src: "http://res.cloudinary.com/hugeinc/image/upload/model-education_x0wqje",
+        alt: "Pulte Planning Center",
+        caption: "Pulte Planning Center",
+      },
+      headline: "Pulte Planning Center",
+      description:
+        "At Pulte, we build our homes with you in mind. Every inch was thoughtfully designed to best meet your family’s needs making your life better, happier and easier. Simply put, you can do more in a Pulte home, because we’ve built in all the ways to get more out of life.",
+    },
+    {
+      tab: "Super Laundry",
+      image: {
+        src: "http://res.cloudinary.com/hugeinc/image/upload/model-education_x0wqje",
+        alt: "Super Laundry",
+        caption: "Super Laundry",
+      },
+      headline: "Super Laundry",
+      description:
+        "At Pulte, we build our homes with you in mind. Every inch was thoughtfully designed to best meet your family’s needs making your life better, happier and easier. Simply put, you can do more in a Pulte home, because we’ve built in all the ways to get more out of life.",
+    },
+    {
+      tab: "Built-In Convenience",
+      image: {
+        src: "http://res.cloudinary.com/hugeinc/image/upload/model-education_x0wqje",
+        alt: "Built-In Convenience",
+        caption: "Built-In Convenience",
+      },
+      headline: "Built-In Convenience",
+      description:
+        "At Pulte, we build our homes with you in mind. Every inch was thoughtfully designed to best meet your family’s needs making your life better, happier and easier. Simply put, you can do more in a Pulte home, because we’ve built in all the ways to get more out of life.",
+    },
+    {
+      tab: "Master with Mini Suite",
+      image: {
+        src: "http://res.cloudinary.com/hugeinc/image/upload/model-education_x0wqje",
+        alt: "Master with Mini Suite",
+        caption: "Master with Mini Suite",
+      },
+      headline: "Master with Mini Suite",
+      description:
+        "At Pulte, we build our homes with you in mind. Every inch was thoughtfully designed to best meet your family’s needs making your life better, happier and easier. Simply put, you can do more in a Pulte home, because we’ve built in all the ways to get more out of life.",
+    },
+    {
+      tab: "Innovative Kitchen",
+      image: {
+        src: "http://res.cloudinary.com/hugeinc/image/upload/model-education_x0wqje",
+        alt: "Innovative Kitchen",
+        caption: "Innovative Kitchen",
+      },
+      headline: "Innovative Kitchen",
+      description:
+        "At Pulte, we build our homes with you in mind. Every inch was thoughtfully designed to best meet your family’s needs making your life better, happier and easier. Simply put, you can do more in a Pulte home, because we’ve built in all the ways to get more out of life.",
+    },
+    {
+      tab: "Abundant Storage",
+      image: {
+        src: "http://res.cloudinary.com/hugeinc/image/upload/model-education_x0wqje",
+        alt: "Abundant Storage",
+        caption: "Abundant Storage",
+      },
+      headline: "Abundant Storage",
+      description:
+        "At Pulte, we build our homes with you in mind. Every inch was thoughtfully designed to best meet your family’s needs making your life better, happier and easier. Simply put, you can do more in a Pulte home, because we’ve built in all the ways to get more out of life.",
+    },
+    {
+      tab: "Feature Fourteen",
+      image: {
+        src: "http://res.cloudinary.com/hugeinc/image/upload/model-education_x0wqje",
+        alt: "Feature Fourteen",
+        caption: "Feature Fourteen",
+      },
+      headline: "Feature Fourteen",
+      description:
+        "At Pulte, we build our homes with you in mind. Every inch was thoughtfully designed to best meet your family’s needs making your life better, happier and easier. Simply put, you can do more in a Pulte home, because we’ve built in all the ways to get more out of life.",
+    },
+    {
+      tab: "Feature Fifteen",
+      image: {
+        src: "http://res.cloudinary.com/hugeinc/image/upload/model-education_x0wqje",
+        alt: "Feature Fifteen",
+        caption: "Feature Fifteen",
+      },
+      headline: "Feature Fifteen",
+      description:
+        "At Pulte, we build our homes with you in mind. Every inch was thoughtfully designed to best meet your family’s needs making your life better, happier and easier. Simply put, you can do more in a Pulte home, because we’ve built in all the ways to get more out of life.",
+    },
+  ],
+};
+
+
+$(document).ready(function () {
+    var Template = $("#DesignedForYou-template").html();
+    var compiledUsersTemp = Handlebars.compile(Template);
+    var $tabs = $(".secondary-tab");
+    var $tabContents = $(".right-content");
+  
+    $tabs.on("click", function () {
+      var tabIndex = $(this).data("index");
+      
+      // Hide all tab contents
+      $tabContents.removeClass("active-tab-content").addClass("inactive-tab-content");
+      
+      // Show the selected tab content
+      $tabContents.eq(tabIndex).removeClass("inactive-tab-content").addClass("active-tab-content");
+  
+      // Update the active class on tabs
+      $tabs.removeClass("is-active");
+      $(this).addClass("is-active");
+    });
+  
+    $(".DesignedForYou").html(compiledUsersTemp(userData));
+  });
+  
